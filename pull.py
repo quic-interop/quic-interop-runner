f3359a342f8a94899e5057b960ad3a8fcdc6ce60
Marten Seemann <martenseemann@gmail.com>
2019-12-23 09:21:46 +0400

    fix the pull script

--- pull.py
+++ pull.py
@@ -4,6 +4,6 @@ from implementations import IMPLEMENTATIONS
 print("Pulling the simulator...")
 os.system("docker pull martenseemann/quic-network-simulator")
 
-for name in IMPLEMENTATIONS:
+for name, value in IMPLEMENTATIONS.items():
   print("\nPulling " + name + "...")
-  os.system("docker pull " + IMPLEMENTATIONS[name])
+  os.system("docker pull " + value["url"])
