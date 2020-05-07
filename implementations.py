c7c5870d5f7d5f7f8d00b72af44fff279205d0b1
Marten Seemann <martenseemann@gmail.com>
2020-05-07 20:55:34 +0700

    fix name of quic-go

--- implementations.py
+++ implementations.py
@@ -1,6 +1,6 @@
 # add your QUIC implementation here
 IMPLEMENTATIONS = {  # name => [ docker image, role ]; role: 0 == 'client', 1 == 'server', 2 == both
-    "quicgo": {"url": "martenseemann/quic-go-interop:latest", "role": 2},
+    "quic-go": {"url": "martenseemann/quic-go-interop:latest", "role": 2},
     "quicly": {"url": "janaiyengar/quicly:interop", "role": 2},
     "ngtcp2": {"url": "ngtcp2/ngtcp2-interop:latest", "role": 2},
     "quant": {"url": "ntap/quant:interop", "role": 2},
