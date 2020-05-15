1fbce230aafda1e51a2b1266e480bfd21e540b5f
Lars Eggert <lars@eggert.org>
2020-05-15 13:00:19 +0300

    Remove duplicates from ciphersuites lists

--- testcases.py
+++ testcases.py
@@ -319,10 +319,10 @@ class TestCaseChaCha20(TestCase):
         for p in self._client_trace().get_initial(Direction.FROM_CLIENT):
             if hasattr(p, "tls_handshake_ciphersuite"):
                 ciphersuites.append(p.tls_handshake_ciphersuite)
-        if len(ciphersuites) != 1 or ciphersuites[0] != "4867":
+        if len(set(ciphersuites)) != 1 or ciphersuites[0] != "4867":
             logging.info(
                 "Expected only ChaCha20 cipher suite to be offered. Got: %s",
-                ciphersuites,
+                set(ciphersuites),
             )
             return False
         return self._check_version_and_files()
