bf5a84b25923b90dbd9cf8064a49c65a4761e6d3
Andy Grover <agrover@mozilla.com>
2020-07-10 11:23:53 -0700

    Indicate neqo now supports server
    Currently only http3 test but more soon.

--- implementations.py
+++ implementations.py
@@ -9,7 +9,7 @@ IMPLEMENTATIONS = {  # name => [ docker image, role ]; role: 0 == 'client', 1 ==
     "kwik": {"url": "peterdoornbosch/kwik_n_flupke-interop", "role": 0},
     "picoquic": {"url": "privateoctopus/picoquic:latest", "role": 2},
     "aioquic": {"url": "aiortc/aioquic-qns:latest", "role": 2},
-    "neqo": {"url": "neqoquic/neqo-qns:latest", "role": 0},
+    "neqo": {"url": "neqoquic/neqo-qns:latest", "role": 2},
     "nginx": {"url": "fiestajetsam/nginx-quic-qns", "role": 1},
     "msquic": {"url": "mcr.microsoft.com/msquic/qns:latest", "role": 1},
 }
