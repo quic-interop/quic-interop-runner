26b8a08ca75f0e31e20a1eada93f2964d1e155ce
Jana Iyengar <jri.ietf@gmail.com>
2020-07-21 18:02:39 -0700

    Change quicly's docker image location

--- implementations.py
+++ implementations.py
@@ -1,7 +1,7 @@
 # add your QUIC implementation here
 IMPLEMENTATIONS = {  # name => [ docker image, role ]; role: 0 == 'client', 1 == 'server', 2 == both
     "quic-go": {"url": "martenseemann/quic-go-interop:latest", "role": 2},
-    "quicly": {"url": "janaiyengar/quicly:interop", "role": 2},
+    "quicly": {"url": "h2oserver/quicly-interop-runner:latest", "role": 2},
     "ngtcp2": {"url": "ngtcp2/ngtcp2-interop:latest", "role": 2},
     "quant": {"url": "ntap/quant:interop", "role": 2},
     "mvfst": {"url": "lnicco/mvfst-qns:latest", "role": 2},
