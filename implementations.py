8a8eb536aacc7af7f74128d40c6f305c9a8be0f6
Marten Seemann <martenseemann@gmail.com>
2020-08-27 16:42:34 +0700

    run the updated version of black to reformat code

--- implementations.py
+++ implementations.py
@@ -2,21 +2,23 @@ import json
 import sys
 
 # add your QUIC implementation here
-IMPLEMENTATIONS = {  # name => [ docker image, role ]; role: 0 == 'client', 1 == 'server', 2 == both
-    "quic-go": {"url": "martenseemann/quic-go-interop:latest", "role": 2},
-    "quicly": {"url": "h2oserver/quicly-interop-runner:latest", "role": 2},
-    "ngtcp2": {"url": "ngtcp2/ngtcp2-interop:latest", "role": 2},
-    "quant": {"url": "ntap/quant:interop", "role": 2},
-    "mvfst": {"url": "lnicco/mvfst-qns:latest", "role": 2},
-    "quiche": {"url": "cloudflare/quiche-qns:latest", "role": 2},
-    "kwik": {"url": "peterdoornbosch/kwik_n_flupke-interop", "role": 0},
-    "picoquic": {"url": "privateoctopus/picoquic:latest", "role": 2},
-    "aioquic": {"url": "aiortc/aioquic-qns:latest", "role": 2},
-    "neqo": {"url": "neqoquic/neqo-qns:latest", "role": 2},
-    "nginx": {"url": "nginx/nginx-quic-qns:latest", "role": 1},
-    "msquic": {"url": "mcr.microsoft.com/msquic/qns:latest", "role": 1},
-    "pquic": {"url": "pquic/pquic-interop:latest", "role": 2},
-}
+IMPLEMENTATIONS = (
+    {  # name => [ docker image, role ]; role: 0 == 'client', 1 == 'server', 2 == both
+        "quic-go": {"url": "martenseemann/quic-go-interop:latest", "role": 2},
+        "quicly": {"url": "h2oserver/quicly-interop-runner:latest", "role": 2},
+        "ngtcp2": {"url": "ngtcp2/ngtcp2-interop:latest", "role": 2},
+        "quant": {"url": "ntap/quant:interop", "role": 2},
+        "mvfst": {"url": "lnicco/mvfst-qns:latest", "role": 2},
+        "quiche": {"url": "cloudflare/quiche-qns:latest", "role": 2},
+        "kwik": {"url": "peterdoornbosch/kwik_n_flupke-interop", "role": 0},
+        "picoquic": {"url": "privateoctopus/picoquic:latest", "role": 2},
+        "aioquic": {"url": "aiortc/aioquic-qns:latest", "role": 2},
+        "neqo": {"url": "neqoquic/neqo-qns:latest", "role": 2},
+        "nginx": {"url": "nginx/nginx-quic-qns:latest", "role": 1},
+        "msquic": {"url": "mcr.microsoft.com/msquic/qns:latest", "role": 1},
+        "pquic": {"url": "pquic/pquic-interop:latest", "role": 2},
+    }
+)
 
 
 def main():
