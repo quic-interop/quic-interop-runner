33b321c6d35a70e8ea4d2b0164a71a11d3b83bcc
Marten Seemann <martenseemann@gmail.com>
2020-09-21 12:32:25 +0700

    reduce verbosity of cert generation script

--- certs.sh
+++ certs.sh
@@ -12,22 +12,22 @@ CERTDIR=$1
 
 mkdir -p $CERTDIR || true
 
-echo "Generating CA key and certificate:"
+# Generate CA key and certificate
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 \
   -keyout $CERTDIR/ca.key -out $CERTDIR/ca.pem \
-  -subj "/O=interop runner Certificate Authority/"
+  -subj "/O=interop runner Certificate Authority/" \
+  2> /dev/null
 
-echo "Generating CSR"
+# Create a CSR
 openssl req -out $CERTDIR/cert.csr -new -newkey rsa:2048 -nodes -keyout $CERTDIR/priv.key \
-  -subj "/O=interop runner/"
+  -subj "/O=interop runner/" \
+  2> /dev/null
 
-echo "Sign certificate:"
+# Sign the certificate
 openssl x509 -req -sha256 -days 365 -in $CERTDIR/cert.csr -out $CERTDIR/cert.pem \
   -CA $CERTDIR/ca.pem -CAkey $CERTDIR/ca.key -CAcreateserial \
-  -extfile <(printf "subjectAltName=DNS:server")
-
-# debug output the certificate
-openssl x509 -noout -text -in $CERTDIR/cert.pem
+  -extfile <(printf "subjectAltName=DNS:server") \
+  2> /dev/null
 
 # we don't need the CA key, the serial number and the CSR any more
 rm $CERTDIR/ca.key $CERTDIR/cert.csr $CERTDIR/ca.srl
