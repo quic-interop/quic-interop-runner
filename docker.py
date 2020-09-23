9b139ca43f188a3430ef7639d14d00b7d6b9cc5b
Marten Seemann <martenseemann@gmail.com>
2020-09-23 18:01:52 +0700

    implement a timeout

--- docker.py
+++ docker.py
@@ -7,9 +7,11 @@ import typing
 class DockerRunner:
     _containers = []
     _cond = None
+    _timeout = 0  # in seconds
 
-    def __init__(self):
+    def __init__(self, timeout: int):
         self._cond = threading.Condition()
+        self._timeout = timeout
 
     def add_container(self, name: str, container: str, env: str):
         self._containers.append({"name": name, "container": container, "env": env})
@@ -31,11 +33,17 @@ class DockerRunner:
             logging.debug("%s container returned.", name)
             self._cond.notify()
 
+    def _run_timer(self):
+        logging.info("Timer expired. Stopping all containers.")
+        with self._cond:
+            self._cond.notify()
+
     def run(self):
         threads = []
         with open("test.log", "w") as f:
             # Start all containers (in separate threads)
             for e in self._containers:
+
                 def run_container():
                     self._execute(
                         "docker run --rm --name {} --env {} {}".format(
@@ -48,20 +56,30 @@ class DockerRunner:
                 t = threading.Thread(target=run_container)
                 t.start()
                 threads.append(t)
+            # set a timer
+            timer = threading.Timer(self._timeout, self._run_timer)
+            timer.start()
+
             # Wait for the first container to exit.
             # Then stop all other docker containers.
             with self._cond:
                 self._cond.wait()
                 names = [x["name"] for x in self._containers]
-                subprocess.run("docker stop " + " ".join(names), shell="True")
+                subprocess.run(
+                    "docker stop " + " ".join(names),
+                    shell="True",
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
             # wait for all threads to finish
             for t in threads:
                 t.join()
+            timer.cancel()
 
 
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
-    r = DockerRunner()
+    r = DockerRunner(10)
     r.add_container("client", "traptest", "DURATION=3")
     r.add_container("server", "traptest", "DURATION=2000")
     r.run()
