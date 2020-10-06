a1df2eed6690add24c0881db8a8f374df47f42c9
Lars Eggert <lars@eggert.org>
2020-10-06 11:18:57 +0300

    Temp commit

--- trace.py
+++ trace.py
@@ -4,8 +4,10 @@ from typing import List, Optional
 
 import pyshark
 
-IP_CLIENT = "193.167.0.100"
-IP_SERVER = "193.167.100.100"
+IP4_CLIENT = "193.167.0.100"
+IP4_SERVER = "193.167.100.100"
+IP6_CLIENT = "fd00:cafe:cafe:0::100"
+IP6_SERVER = "fd00:cafe:cafe:100::100"
 
 
 class Direction(Enum):
@@ -33,9 +35,9 @@ WIRESHARK_PACKET_TYPES = {
 
 
 def get_direction(p) -> Direction:
-    if p.ip.src == IP_CLIENT:
+    if p.ip.src == IP4_CLIENT:
         return Direction.FROM_CLIENT
-    if p.ip.src == IP_SERVER:
+    if p.ip.src == IP4_SERVER:
         return Direction.FROM_SERVER
     return Direction.INVALID
 
@@ -59,9 +61,9 @@ class TraceAnalyzer:
     def _get_direction_filter(self, d: Direction) -> str:
         f = "(quic && !icmp) && "
         if d == Direction.FROM_CLIENT:
-            return f + "ip.src==" + IP_CLIENT + " && "
+            return f + "(ip.src==" + IP4_CLIENT + " || ipv6.src==" + IP6_CLIENT + ") && "
         elif d == Direction.FROM_SERVER:
-            return f + "ip.src==" + IP_SERVER + " && "
+            return f + "(ip.src==" + IP4_SERVER + " || ipv6.src==" + IP6_SERVER + ") && "
         else:
             return f
 
