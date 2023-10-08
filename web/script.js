7ced844a976af0279e20b4f1634fcd8907bf6e97
Marten Seemann <martenseemann@gmail.com>
2023-10-08 16:42:58 +0700

    get files from Backblaze

--- web/script.js
+++ web/script.js
@@ -2,6 +2,7 @@
 
 (function() {
   "use strict";
+  const host = "https://quic-interop-runner.s3.us-east-005.backblazeb2.com";
   const map = { client: {}, server: {}, test: {} };
   const color_type = { succeeded: "success", unsupported: "secondary disabled", failed: "danger"};
 
@@ -272,7 +273,7 @@
     document.getElementsByTagName("body")[0].classList.add("loading");
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    xhr.open('GET', 'logs/' + dir + '/result.json');
+    xhr.open('GET', `${host}/${dir}/result.json`);
     xhr.onreadystatechange = function() {
       if(xhr.readyState !== XMLHttpRequest.DONE) return;
       if(xhr.status !== 200) {
@@ -290,7 +291,7 @@
   // enable loading of old runs
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
-  xhr.open('GET', 'logs/logs.json');
+  xhr.open('GET', `${host}/logs.json`);
   xhr.onreadystatechange = function() {
     if(xhr.readyState !== XMLHttpRequest.DONE) return;
     if(xhr.status !== 200) {
