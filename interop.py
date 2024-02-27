149e19789c5c781beaf568d6852f9c203e5cd823
Max Inden <mail@max-inden.de>
2024-02-27 01:44:58 +0100

    print result symbol in addition to color (#354)
    * feat: print result symbol in addition to color
    
    Previously the test abbreviation was printed in green, grey and red depending on
    the result. Colors are not available in all environments, e.g. when printing
    into a file.
    
    This commit adds symbols in addition to colors.
    
    +------+--------+
    |      |  neqo  |
    +------+--------+
    | neqo | ✓(H,S) |
    |      |  ?()   |
    |      |  ✕(R)  |
    +------+--------+
    
    * yapf fmt

--- interop.py
+++ interop.py
@@ -180,8 +180,13 @@ class InteropRunner:
         logging.info("Run took %s", datetime.now() - self._start_time)
 
         def get_letters(result):
-            return "".join(
-                [test.abbreviation() for test in cell if cell[test] is result]
+            return (
+                result.symbol()
+                + "("
+                + ",".join(
+                    [test.abbreviation() for test in cell if cell[test] is result]
+                )
+                + ")"
             )
 
         if len(self._tests) > 0:
