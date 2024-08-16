0c8927e8a3dedd4de765c50958acaa0ef722fbbd
Lars Eggert <lars@eggert.org>
2024-08-16 15:38:09 +0100

    Use fallback `tcpdump` pcap
    If the pacps generates from within ns3 are empty, fall back to using
    ones generated via tcpdump.
    
    Needs https://github.com/quic-interop/quic-network-simulator/pull/133

--- testcases.py
+++ testcases.py
@@ -160,16 +160,18 @@ class TestCase(abc.ABC):
 
     def _client_trace(self):
         if self._cached_client_trace is None:
-            self._cached_client_trace = TraceAnalyzer(
-                self._sim_log_dir.name + "/trace_node_left.pcap", self._keylog_file()
-            )
+            path = self._sim_log_dir.name + "/trace_node_left.pcap"
+            if os.path.getsize(path):
+                path = self._sim_log_dir.name + "/tcpdump_left.pcap"
+            self._cached_client_trace = TraceAnalyzer(path, self._keylog_file())
         return self._cached_client_trace
 
     def _server_trace(self):
         if self._cached_server_trace is None:
-            self._cached_server_trace = TraceAnalyzer(
-                self._sim_log_dir.name + "/trace_node_right.pcap", self._keylog_file()
-            )
+            path = self._sim_log_dir.name + "/trace_node_right.pcap"
+            if os.path.getsize(path):
+                path = self._sim_log_dir.name + "/tcpdump_right.pcap"
+            self._cached_server_trace = TraceAnalyzer(path, self._keylog_file())
         return self._cached_server_trace
 
     # see https://www.stefanocappellini.it/generate-pseudorandom-bytes-with-python/ for benchmarks
